987e19e1b6f12a6e2d7589004e044859ae0598eb
Md. Niamul Haque <haque2305101156@diu.edu.bd>
2024-11-19 22:53:00 +0600

    Create tic_tac_toe_2.c

---
+++ tic_tac_toe_2.c
@@ -0,0 +1,12 @@
+void printBoard(char board[3][3]) {
+    printf("\nTic-Tac-Toe Board:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf(" %c ", board[i][j]);
+            if (j < 2) printf("|");
+        }
+        printf("\n");
+        if (i < 2) printf("---|---|---\n");
+    }
+    printf("\n");
+}
