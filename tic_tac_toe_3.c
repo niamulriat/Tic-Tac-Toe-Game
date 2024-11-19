3157db91b16b9da617ea8c684f2fc01789b9c295
Md. Niamul Haque <haque2305101156@diu.edu.bd>
2024-11-19 22:54:16 +0600

    Create tic_tac_toe_3.c

---
+++ tic_tac_toe_3.c
@@ -0,0 +1,15 @@
+int checkWinner(char board[3][3])
+{
+    for (int i = 0; i < 3; i++) 
+    {
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ')
+            return 1;
+        if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ')
+            return 1;
+    }
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ')
+        return 1;
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ')
+        return 1;
+    return 0;
+}
